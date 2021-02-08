5e9d7ba4ac3ef54f18cc56328084496017589fde
lanbazz <62804907+lanbazz@users.noreply.github.com>
2021-02-08 05:10:35 +0100

    SECOND COMMIT

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ async function join() {
   
   // play local video track
   localTracks.videoTrack.play("local-player");
-  $("#local-player-name").text(`localVideo(${options.uid})`);
+  $("#local-player-name").text(`Me`);
 
   // publish local tracks to channel
   await client.publish(Object.values(localTracks));
@@ -108,7 +108,7 @@ async function subscribe(user, mediaType) {
   if (mediaType === 'video') {
     const player = $(`
       <div id="player-wrapper-${uid}">
-        <p class="player-name">remoteUser(${uid})</p>
+        <p class="player-name"> Other Participant </p>
         <div id="player-${uid}" class="player"></div>
       </div>
     `);
