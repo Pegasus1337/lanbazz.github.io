52b92d862b157c5a4bec005196fbcb20eb625ef6
lanbazz <62804907+lanbazz@users.noreply.github.com>
2021-02-10 01:03:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ $("#join-form").submit(async function (e) {
 
 $("#leave").click(function (e) {
   leave();
-  window.location.href = "https://google.com";
+  window.location.href = "http://localhost:4200/call";
 })
 
 async function join() {
